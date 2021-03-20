78833eedb6e3ac286085a5a430544a14cc13dd9c
kani-tech <kanishksk@gmail.com>
2021-03-20 10:38:37 -0400

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,25 @@ import axios from 'axios'
 import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom"
 // Pages
 import registrationScreen from "./pages/registration.jsx"
+import homePage from "./pages/home.jsx"
+import Errpage from "./pages/errpage.jsx"
+import LoginPage from "./pages/login.jsx"
+
 
 function App() {
+  const [token, setToken] = useState('')
 
+  if (!token) {
+    return <LoginPage setToken={setToken} />
+  }
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/" component={homePage} />
         <Route exact path="/register" component={registrationScreen} />
-        <Route exact path="404" component={errPage}
+        <Route exact path="/login" component={LoginPage} />
+        <Route component={Errpage} />
+        <Redirect to="/404" />
       </Switch>
     </BrowserRouter>
   )
