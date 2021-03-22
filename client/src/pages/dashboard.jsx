7711b4a2ba39296ba8896eca934456f3e2f9a711
kani-tech <kanishksk@gmail.com>
2021-03-22 12:11:37 -0400

    update dashboard
    kind of logs out testing required
    
    Co-Authored-By: Het7 <34385993+het7@users.noreply.github.com>
    Co-Authored-By: arashbayat1 <65373140+arashbayat1@users.noreply.github.com>

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
-
-
+import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom"
 function Dashboard() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [user, setUser] = useState();
 
+    const handleLogout = () => {
+        setUser({});
+        setEmail("");
+        setPassword("");
+        localStorage.clear();
+    };
     return (
-        <h1> Welcome to da HUB </h1>
+        <div>
+            <h1> Welcome to da HUB {user} </h1>
+            <Link to="/">
+                <button onClick={handleLogout}>logout</button>
+            </Link>
+        </div>
+
     )
 }
 
