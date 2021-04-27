455ecf9171a47ee24a17adc6839f3430b18036ea
kani-tech <kanishksk@gmail.com>
2021-04-27 13:03:34 -0400

    add new models
    Co-Authored-By: Het7 <34385993+Het7@users.noreply.github.com>
    Co-Authored-By: arashbayat1 <65373140+arashbayat1@users.noreply.github.com>

---
+++ models/chores.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const choreSchema = new Schema({
+    roomKey: {
+        type: String,
+        required: true
+    },
+    Item: {
+        type: String,
+        required: true,
+    },
+    Name: {
+        type: String,
+        required: true,
+    },
+})
+
+const choreModel = mongoose.model('chore', choreSchema);
+
+
+module.exports = choreModel;
---
+++ models/requestModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const requestSchema = new Schema({
+    roomKey: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    request: {
+        type: String,
+        required: true,
+    }
+})
+
+const requestModel = mongoose.model('request', requestSchema);
+
+
+module.exports = requestModel;
