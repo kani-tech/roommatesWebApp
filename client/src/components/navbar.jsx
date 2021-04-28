2059416344e255723e2462f12d6d040014167d5f
kani-tech <kanishksk@gmail.com>
2021-04-27 22:13:06 -0400

    rentcard
    Co-Authored-By: arashbayat1 <65373140+arashbayat1@users.noreply.github.com>
    Co-Authored-By: Het7 <34385993+Het7@users.noreply.github.com>

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,28 +5,30 @@ import { Form, Button, FormControl, Nav, NavDropdown } from 'react-bootstrap'
 import Dashboard from '../pages/dashboard.jsx'
 
 
-const MyNavBar = () => (
+const MyNavBar = (props) => (
 
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="light" expand="lg" width="40rem">
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
                 <Nav.Link href="/dashboard">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="/chores">Tasks</Nav.Link>
+                <Nav.Link href="/complaints">Requests</Nav.Link>
+                <Nav.Link href="/payrent">Finances</Nav.Link>
 
 
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                     <NavDropdown.Item href="/complaints">Complaints</NavDropdown.Item>
                     <NavDropdown.Item href="/chores">Chores</NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
             </Nav>
             <Form inline>
                 <Link to="/">
-                    <button onClick={Dashboard.handleLogout}>Logout</button>
+                    <button onClick={props.logout}>Logout</button>
                 </Link>
 
 
@@ -34,8 +36,8 @@ const MyNavBar = () => (
         </Navbar.Collapse>
 
     </Navbar>
-
 )
+
 
 export default MyNavBar
 
---
+++ client/src/components/rentcard.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom"
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { Form, Button, FormControl, Nav, NavDropdown } from 'react-bootstrap'
+import Dashboard from '../pages/dashboard'
+const RentCard = (props) => (
+
+    <Card style={{ width: '25rem' }, { height: '20rem' }}>
+        <Card.Body>
+            <Card.Title>Finance Information</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Here's the update for this month</Card.Subtitle>
+            <ListGroup variant="flush">
+                <ListGroup.Item>You owe: {props.rent}</ListGroup.Item>
+                <ListGroup.Item>By: {props.date}</ListGroup.Item>
+            </ListGroup>
+
+
+        </Card.Body>
+    </Card>
+
+)
+
+export default RentCard
+
+
+
