e48a792fea99a122e706e958e0f4247689bdbfb1
arashbayat1 <65373140+dasharash@users.noreply.github.com>
2021-05-20 16:04:07 -0400

    Update Style

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,40 +1,34 @@
-import React from "react";
-import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom"
-import '../stylesheets/home.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import {BrowserRouter, Route, Switch, Link, Redirect} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../stylesheets/home.css';
 // Pages
 
-
 const homePage = () => {
+  return (
+    <div class="home-body" id="box_home">
+      <div class="boxed-stuff">
+        <div class="text">
+          <h1 class="cover-heading">Roomates</h1>
+          <p class="lead">Bringing students together.</p>
+        </div>
 
-    return (
-        <div class="home-body" id="box_home">
+        <Link to="/login">
+          <button type="button" class="btn btn-lg btn-secondary" id="home-btn">
+            Login
+          </button>
+        </Link>
+        <Link to="/register">
+          <button type="button" class="btn btn-lg btn-secondary" id="home-btn">
+            Register
+          </button>
+        </Link>
+      </div>
 
-            <div class="boxed-stuff">
-                <div class="text">
-                    <h1 class="cover-heading">Roomates</h1>
-                    <p class="lead">Bringing students together.</p>
-                </div>
-
-                <Link to="/login">
-                    <button type="button" class="btn btn-lg btn-secondary" id="home-btn">
-                        Login
-                    </button>
-                </Link>
-                <Link to="/register">
-                    <button type="button" class="btn btn-lg btn-secondary" id="home-btn">
-                        Register
-                    </button>
-                </Link>
-            </div>
-
-            <footer class="mastfoot mt-auto">
-                <div class="inner">
-
-                </div>
-            </footer>
-        </div >
-
-    )
-}
+      <footer class="mastfoot mt-auto">
+        <div class="inner"></div>
+      </footer>
+    </div>
+  );
+};
 export default homePage;
