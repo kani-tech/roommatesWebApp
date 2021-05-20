e78c1ec99ac6624b0d36a553da44f783cac3f01c
arashbayat1 <65373140+dasharash@users.noreply.github.com>
2021-05-20 16:11:32 -0400

    Update Title
    Co-Authored-By: Kanishk Kashyap <64444910+kani-tech@users.noreply.github.com>
    Co-Authored-By: Het7 <34385993+het7@users.noreply.github.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
