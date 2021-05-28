5b36d3082f129f70ba21d49013a5934ffea831fd
arashbayat1 <65373140+dasharash@users.noreply.github.com>
2021-05-28 10:54:57 -0400

    Fixed Complaints error page
    Co-Authored-By: Kanishk Kashyap <64444910+kani-tech@users.noreply.github.com>
    Co-Authored-By: Het7 <34385993+het7@users.noreply.github.com>

--- client/src/components/landlordnavbar.jsx
+++ client/src/components/landlordnavbar.jsx
@@ -1,42 +1,29 @@
-import React, { useState } from 'react'
-import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom"
-import Navbar from 'react-bootstrap/Navbar'
-import { Form, Button, FormControl, Nav, NavDropdown } from 'react-bootstrap'
-import '../stylesheets/navbar.css'
-
+import React, {useState} from 'react';
+import {BrowserRouter, Route, Switch, Link, Redirect} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import {Form, Button, FormControl, Nav, NavDropdown} from 'react-bootstrap';
+import '../stylesheets/navbar.css';
 
 const LandlordNavBar = (props) => (
-    <div className="navbar" fluid>
-        <Navbar bg="light" expand="lg" >
+  <div className="navbar" fluid>
+    <Navbar bg="light" expand="lg">
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto nav-text">
+          <Nav.Link href="/dashboard">Home</Nav.Link>
+          <Nav.Link href="/addroom">Add Room</Nav.Link>
+          <Nav.Link href="/l_complaints">Complaints</Nav.Link>
+        </Nav>
 
-            <Navbar.Collapse id="basic-navbar-nav">
+        <Link to="/">
+          <button onClick={props.logout} className="logout">
+            Logout
+          </button>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
+  </div>
+);
 
-                <Nav className="mr-auto nav-text">
-                    <Nav.Link href="/dashboard">Home</Nav.Link>
-                    <Nav.Link href="/addroom">Add Room</Nav.Link>
-                    <Nav.Link href="/payrent">Complaints</Nav.Link>
-                </Nav>
-
-
-
-                <Link to="/">
-                    <button onClick={props.logout} className="logout">Logout</button>
-                </Link>
-
-
-
-            </Navbar.Collapse>
-
-        </Navbar>
-
-    </div>
-
-)
-
-
-export default LandlordNavBar
-
-
-
+export default LandlordNavBar;
